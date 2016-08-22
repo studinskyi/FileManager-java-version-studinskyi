a9b31668299983964524f2f690ba9e5e4728aaa3
studinskyi <studinskyi_dv@mail.ru>
2016-08-22 23:20:13 +0300

    Initial commit

---
+++ src/com/qa/studinskyi_1lec/File.java
@@ -0,0 +1,10 @@
+package com.qa.studinskyi_1lec;
+
+/**
+ * Created by dima on 22.08.2016.
+ */
+public class File {
+    public File() {
+    }
+
+}
---
+++ src/com/qa/studinskyi_1lec/FileManager.java
@@ -0,0 +1,27 @@
+package com.qa.studinskyi_1lec;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FileManager {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("0 - выход");
+        System.out.println("1 - просмотр фалйлов");
+        System.out.println("2 - создать файл");
+        System.out.println("3 - удалить файл");
+        System.out.println("4 - переименовать файл");
+        String command = reader.readLine();
+        while (!command.equals("0")) {
+            if (command.equals("1")) {
+                System.out.println("запуск просмотра списка файлов");
+            } else if(command.equals("2"))  {
+                System.out.println("запуск создания файла");
+            }
+            command = reader.readLine();
+        }
+        System.out.println("Работа программы файлового менеджера завершена!");
+    }
+}
